dbd82c82704408510ab705ed60a13c30adedb07a
hs.hwang <hs.hwang@smartstudygames.com>
2021-05-05 11:03:58 +0900

    no message

--- Client/Assets/Scripts/MainScene.cs
+++ Client/Assets/Scripts/MainScene.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Grpc.Core;
 
 public class MainScene : MonoBehaviour
 {  
+    Channel m_Channel;
+
+    void Start()
+    {
+        Connect();
+    }
+
+    void Connect()
+    {
+        m_Channel = new Channel("localhost", ChannelCredentials.Insecure);
+    }
+
     public void OnClick() => Debug.Log("haha");
 }
