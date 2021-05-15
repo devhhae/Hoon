0c7fa8aebf4d8f8eb39f789d124ccc926cbe7193
hs.hwang <hs.hwang@smartstudygames.com>
2021-05-16 00:55:42 +0900

    setting

--- Client/Assets/Scripts/MainScene.cs
+++ Client/Assets/Scripts/MainScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Grpc.Core;
 
 public class MainScene : MonoBehaviour
-{  
+{
     Channel m_Channel;
 
     void Start()
@@ -14,7 +14,21 @@ public class MainScene : MonoBehaviour
 
     void Connect()
     {
-        m_Channel = new Channel("localhost", ChannelCredentials.Insecure);
+        try
+        {
+            m_Channel = new Channel("localhost", 5000, ChannelCredentials.Insecure);
+            m_Channel.ConnectAsync();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    void Update()
+    {
+        if (m_Channel == null) return;
+        Debug.Log($"Channel State: {m_Channel.State}");
     }
 
     public void OnClick() => Debug.Log("haha");
